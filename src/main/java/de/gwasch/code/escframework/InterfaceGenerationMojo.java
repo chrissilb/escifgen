9698df3f83b077896b3462c8daa6bac638a79191
Christian Silberbauer <40714911+chrissilb@users.noreply.github.com>
2023-09-28 20:20:21 +0200

    Add files via upload

---
+++ src/main/java/de/gwasch/code/escframework/InterfaceGenerationMojo.java
@@ -0,0 +1,89 @@
+package de.gwasch.code.escframework;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import de.gwasch.code.escframework.components.utils.CodeGenerator;
+
+@Mojo(name = "ifgen")
+public class InterfaceGenerationMojo extends AbstractMojo {
+
+	private final static String DEFAULT_INPUT_FOLDER = "src/main/java";
+	private final static String DEFAULT_OUTPUT_FOLDER = "src/main/gen";
+
+	@Parameter(property = "systeminfo.inputFolder", defaultValue = DEFAULT_INPUT_FOLDER)
+	private String inputFolder;
+
+	@Parameter(property = "systeminfo.basePackageName")
+	private String basePackageName;
+
+	@Parameter(property = "systeminfo.outputFolder", defaultValue = DEFAULT_OUTPUT_FOLDER)
+	private String outputFolder;
+	
+    @Parameter( readonly = true, defaultValue = "${project}" )
+    private MavenProject project;
+
+	private void generate() {
+		
+		if (basePackageName == null) {
+			basePackageName = project.getGroupId();
+		}
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream pos = new PrintStream(baos);
+		System.setOut(pos);
+				
+		try {
+			CodeGenerator generator = new CodeGenerator(inputFolder, basePackageName, outputFolder);
+			generator.generateInterfaces();
+			
+			String[] lines = baos.toString().split(System.lineSeparator());
+			for (String line : lines) {
+				getLog().info(line);
+			}
+		}
+		catch(Throwable t) {
+			String[] lines = baos.toString().split(System.lineSeparator());
+			for (String line : lines) {
+				getLog().info(line);
+			}
+			
+			getLog().error(t);
+		}
+		
+		project.addCompileSourceRoot(outputFolder);
+	}
+
+	public void execute() {
+		generate();
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		InterfaceGenerationMojo genPlugin = new InterfaceGenerationMojo();
+
+		if (args.length > 1) {
+			genPlugin.inputFolder = args[0];
+		} 
+		else {
+			genPlugin.inputFolder = DEFAULT_INPUT_FOLDER;
+		}
+		if (args.length > 2) {
+			genPlugin.basePackageName = args[1];
+		} 
+		if (args.length > 3) {
+			genPlugin.outputFolder = args[2];
+		} 
+		else {
+			genPlugin.outputFolder = DEFAULT_OUTPUT_FOLDER;
+		}
+
+		genPlugin.generate();
+	}
+}
